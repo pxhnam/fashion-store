89d4b1bc003180319d126e306a1fd5f7f11e65a7
Phan Xuan Hoai Nam <pxhnam@gmail.com>
2025-08-03 11:14:31 +0700

    feat: add findById method to VariantService for retrieving variants by ID

--- server/src/main/java/com/example/demo/service/VariantService.java
+++ server/src/main/java/com/example/demo/service/VariantService.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Variant;
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.repository.VariantRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class VariantService {
     private final VariantRepository variantRepository;
+
+    public Variant findById(UUID id) {
+        return variantRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Variant not found"));
+    }
 
     public void deleteById(UUID id) {
         variantRepository.deleteById(id);
